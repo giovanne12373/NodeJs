7e125d834b049dce87606d963232a775a2cc5488
Giovanne Henrique Vieira da Silva - 821149648 <821149648@ulife.com.br>
2022-09-12 21:30:29 -0300

    segundo commit

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const express = require ('express');
+const bodyParser = require('body-parser');
+const usuario = require ('./usuario.json');
 const app = express();
-app.use(express.json());
-const usuario = {
-    id: 123456,
-    nome: "giovanne",
-    email: "giovanne@gmail.com",
-}
+app.use(bodyParser.json());
+
+app.get('/rede/usuario', (req, res) => {
+        res.json(usuario);
+     });
+
 
 app.listen(4000, () => {
     console.log("Porta: 4000");
+    console.log(usuario)
 });
